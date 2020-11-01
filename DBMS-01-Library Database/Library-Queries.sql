bfba5ac19698350f540bdf08668f7e7a693cd2de
teamkaliyona <teamkaliyona@gmail.com>
2020-11-01 20:14:48 +0000

    Updating Library Database scripts

--- DBMS-01-Library Database/Library-Queries.sql
+++ DBMS-01-Library Database/Library-Queries.sql
@@ -1,5 +1,5 @@
 --Retrieve details of all books in the library – id, title, name of publisher, authors,
-number of copies in each Programme, etc. 
+--number of copies in each Programme, etc. 
 
 SELECT B.BOOK_ID, B.TITLE, B.PUB_NAME, A.AUTHOR_NAME,C.NO_OF_COPIES,L.PROGRAMME_ID
 FROM BOOK B, BOOK_AUTHORS A, BOOK_COPIES C, LIBRARY_PROGRAMME L
@@ -10,7 +10,7 @@ AND L.PROGRAMME_ID=C.PROGRAMME_ID;
 ---------------------------------------------
 
 --Get the particulars of borrowers who have borrowed more than 3 books, but
-from Jan 2017 to Jun 2017. 
+--from Jan 2017 to Jun 2017. 
 
 SELECT CARD_NO
 FROM BOOK_LENDING
@@ -21,7 +21,7 @@ HAVING COUNT(*)>3;
 ---------------------------------------------
 
 --Delete a book in BOOK table. Update the contents of other tables to reflect this
-data manipulation operation. 
+--data manipulation operation. 
 
 DELETE FROM BOOK
 WHERE BOOK_ID=3; 
@@ -33,7 +33,7 @@ SELECT * FROM BOOK_AUTHORS;
 ---------------------------------------------
 
 --Partition the BOOK table based on year of publication. Demonstrate its working
-with a simple query. 
+--with a simple query. 
 
 CREATE VIEW V_PUBLICATION AS SELECT
 PUB_YEAR
@@ -44,7 +44,7 @@ SELECT * FROM V_PUBLICATION;
 ---------------------------------------------
 
 --Create a view of all books and its number of copies that are currently available
-in the Library.
+--in the Library.
 
 CREATE VIEW V_BOOKS AS
 SELECT B.BOOK_ID, B.TITLE, C.NO_OF_COPIES
